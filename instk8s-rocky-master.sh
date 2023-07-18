ff0cb582155560492d03c19944a7e9367dc7ff12
Vladlin Moiseenko <mvladlin@gmail.com>
2023-07-18 16:58:48 +0300

    edit 8

--- instk8s-rocky-master.sh
+++ instk8s-rocky-master.sh
@@ -1,14 +1,70 @@
 #!/usr/bin/env bash
 
-hostnamectl set-hostname kubemaster-01.centlinux.com
+echo "SELINUX=permissive"
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-echo 192.168.247.147 kubemaster-01.centlinux.com kubemaster-01 >> /etc/hosts
+echo "Set module"
+modprobe overlay
+modprobe br_netfilter
 
-dnf makecache --refresh
+cat > /etc/modules-load.d/k8s.conf << EOF
+overlay
+br_netfilter
+EOF
 
-dnf update -y
+cat > /etc/sysctl.d/k8s.conf << EOF
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
 
-reboot
+sysctl --system
+
+echo "Swap off"
+swapoff -a
+sed -e '/swap/s/^/#/g' -i /etc/fstab
+
+echo "Install Containerd"
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf makecache
+dnf install -y containerd.io
+mv /etc/containerd/config.toml /etc/containerd/config.toml.orig
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false$/SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl enable --now containerd.service
+
+echo "Set firewall"
+firewall-cmd --permanent --add-port={6443,2379,2380,10250,10251,10252}/tcp
+firewall-cmd --reload
+
+echo "Install k8s"
+cat > /etc/yum.repos.d/k8s.repo << EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+dnf makecache
+dnf install -y {kubelet,kubeadm,kubectl} --disableexcludes=kubernetes
+
+systemctl enable --now kubelet.service
+
+echo "Set bash_completion"
+source <(kubectl completion bash)
+kubectl completion bash > /etc/bash_completion.d/kubectl
+
+echo "Install Flannel CNI"
+mkdir /opt/bin 
+curl -fsSLo /opt/bin/flanneld https://github.com/flannel-io/flannel/releases/download/v0.20.1/flannel-v0.20.1-linux-amd64.tar.gz
+chmod +x /opt/bin/flanneld
+
+echo "Init Kubernetes"
+kubeadm config images pull
 
 echo "exit 0"
 exit 0
